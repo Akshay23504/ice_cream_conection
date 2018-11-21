e274cb78bf82bbd235be347eb863c18ea158e179
Akshay23504 <aaravind@syr.edu>
2018-11-20 20:11:30 -0500

    Removed the import statement -.-'

--- se_fall18/settings.py
+++ se_fall18/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import se_fall18.settings_confidential as sc
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
